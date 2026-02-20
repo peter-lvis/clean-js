b9cb2d012c1c73172136fe2d30014d24dc1a17f8
Peter-Lvis <uchepeter990gmail.com>
2026-02-20 14:00:59 +0100

    update script.js

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
+let age = 16;
+let msg = (age>= 18)? "You are an Adult" : "You are a Minor";
+console.log(msg);
+ //For loop
+for (let i = 1; i <= 10; i++) {
+    console.log("uflowstack");
+    ;
+}
+    let coding = ["Javacsript", "ReactJS", "NodeJs" ]
 
-console.log("i love javascript");
+    for (let i = 0; i < coding.length; i++){
+        console.log(coding[i]);
+        
+    }
+
+
--- test.js
+++ test.js
@@ -1,3 +1,38 @@
-console.log('it works');
-console.log("it didnt work");
+// Switch statement 
+let DayName = 6;
+switch (DayName) {
+    case 1:
+        DayName = "Sunday";
+        break;
+        case 2:
+        DayName = "Monday";
+        break;
+        case 3:
+        DayName = "Tuesday";
+        break;
+
+        case 4:
+        DayName = "Wednesday";
+        break;
+
+        case 5:
+        DayName = "Thursday";
+        break;
+
+        case 6:
+        DayName = "Friday";
+        break;
+
+        case 7:
+        DayName = "Saturday";
+        break;
+
+
+    default:
+        DayName = "Invalid Day Number"
+        break;
+}
+
+console.log("The day is: "+ DayName);
+
 
